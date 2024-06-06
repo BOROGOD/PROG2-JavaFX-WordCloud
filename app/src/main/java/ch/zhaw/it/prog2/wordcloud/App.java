edd2176420e20eda6df66bb12e4ecb3710b329b3
Nicola <nicola@me1er.com>
2024-06-07 00:57:58 +0200

    Added fxml file and the controller

--- app/src/main/java/ch/zhaw/it/prog2/wordcloud/App.java
+++ app/src/main/java/ch/zhaw/it/prog2/wordcloud/App.java
@@ -3,12 +3,15 @@
  */
 package ch.zhaw.it.prog2.wordcloud;
 
+import javafx.application.Application;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        Application.launch(MainWindow.class, args);
     }
 }
